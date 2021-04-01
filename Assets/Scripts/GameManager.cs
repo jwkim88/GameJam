cd4ce8650c75b3a3009553ca3476a343d9330949
jwkim88 <jwkim88@gmail.com>
2021-04-01 17:13:31 -0400

    death certificate text implemented

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,6 +20,7 @@ public class GameManager : MonoBehaviour
         officePanel.UpdateYear(year);
         officePanel.HeavenAvailability(heavenAvailability);
         officePanel.HellAvailability(hellAvailability);
+        characterPanel.HideCharacter();
     }
 
     public void OnTimePass()
--- Assets/Scripts/UI_CharacterPanel.cs
+++ Assets/Scripts/UI_CharacterPanel.cs
@@ -2,19 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UI_CharacterPanel : MonoBehaviour
 {
     public Image portrait;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI deathText;
+    [SerializeField] protected CharacterManager cm;
+    [SerializeField] protected RectTransform deathCertificateParent;
     public void ShowCharacter(CharacterData cd)
     {
         portrait.sprite = cd.sprite;
         portrait.SetNativeSize();
         portrait.enabled = true;
+        nameText.text = cd.name;
+        Debug.Log("Searching for " + cd.death);
+        DeathData dd = cm.deaths.Find(d => d.id == cd.death);
+        deathText.text = dd.desc;
+        nameText.enabled = true;
+        deathText.enabled = true;
+        deathCertificateParent.gameObject.SetActive(true);
     }
 
     public void HideCharacter()
     {
         portrait.enabled = false;
+        deathText.enabled = false;
+        nameText.enabled = false;
+        deathCertificateParent.gameObject.SetActive(false);
     }
 }
