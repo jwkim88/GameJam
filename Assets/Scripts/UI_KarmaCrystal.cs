ed13dd234776030ad60ae33b6c65df3a200aaaaf
jwkim88 <jwkim88@gmail.com>
2021-04-02 14:23:57 -0400

    karma crystal has lerp animation

--- Assets/Scripts/UI_KarmaCrystal.cs
+++ Assets/Scripts/UI_KarmaCrystal.cs
@@ -6,10 +6,26 @@ using UnityEngine.UI;
 public class UI_KarmaCrystal : MonoBehaviour
 {
     [SerializeField] private Image crystalFill;
-
+    Coroutine fillCoroutine;
     public void SetFill(float amount)
     {
-        crystalFill.fillAmount = amount;
-        crystalFill.color = Color.Lerp(Color.red, Color.white, amount);
+        if (fillCoroutine != null) StopCoroutine(fillCoroutine);
+        fillCoroutine = StartCoroutine(FillOverTime(amount));
+    }
+
+    IEnumerator FillOverTime(float amount)
+    {
+        float duration = 0.5f;
+        float time = 0;
+        float normalizedTime = 0;
+        float startAmount = crystalFill.fillAmount;
+        while(time < duration)
+        {
+            time += Time.deltaTime;
+            normalizedTime = time / duration;
+            crystalFill.fillAmount = Mathf.Lerp(startAmount, amount, normalizedTime);
+            crystalFill.color = Color.Lerp(Color.red, Color.white, crystalFill.fillAmount);
+            yield return null;
+        }
     }
 }
